7c2014ac9b113b3f5d753b3c7a60c04bf8337ffb
Johan Hawkins <johanhawkins14@gmail.com>
2025-11-22 02:41:35 -0500

    Add Imprimir array de split Add funcion contardor de palabras 5+

--- Intermedio/variables, tipos de datos, funciones, listas, condicionales/ContarPalabras.py
+++ Intermedio/variables, tipos de datos, funciones, listas, condicionales/ContarPalabras.py
@@ -1,8 +1,20 @@
-# Solicitar una frase al usuario
+#Frase del usuario
 frase = input("Por favor ingresa una frase: ")
 
-# Separar la frase en palabras y guardarlas en una lista
+# Separar la frase en palabras
 lista_palabras = frase.split()
+print(lista_palabras)
 
-# Mostrar la lista (opcional)
-print("Lista de palabras:", lista_palabras)
+# Función cuántas palabras tienen más de 5 letras
+def contar_palabras_largas(palabras):
+    contador = 0
+    for palabra in palabras:
+        if len(palabra) > 5:
+            contador += 1
+    return contador
+
+# Uso de la función
+total_largas = contar_palabras_largas(lista_palabras)
+
+# Mostrar resultado
+print("Cantidad de palabras con más de 5 letras:", total_largas)
